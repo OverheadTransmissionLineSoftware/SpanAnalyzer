da74537b008678bbaae93ad6cdd4b1e80c637c50
bretzel12 <bretzel12@users.noreply.github.com>
2015-09-23 21:36:34 -0700

    Preliminary edits for making the form compatible for both Metric and Imperial unit systems.

--- include/cable_editor_dialog.h
+++ include/cable_editor_dialog.h
@@ -4,13 +4,7 @@
 #ifndef OTLS_SPANANALYZER_CABLEEDITORDIALOG_H_
 #define OTLS_SPANANALYZER_CABLEEDITORDIALOG_H_
 
-//***** TEMPORARY
-enum class UnitSystem {
-  Imperial,
-  Metric
-};
-//***** TEMPORARY
-
+#include "models/base/units.h"
 #include "models/transmissionline/cable.h"
 #include "wx/wx.h"
 
@@ -18,16 +12,22 @@ enum class UnitSystem {
 ///
 /// This class is a transmission cable editor dialog.
 ///
-/// \par OVERVIEW
+/// \par UNITS
 ///
 /// This class supports both Imperial and Metric unit systems. This class also
-/// supports similar-system unit conversions (ex: feet->inches) from the cable
-/// data to the dialog controls. This is done so the user can input using
-/// preferred units while maintaining compatibility with the consistent unit
-/// calculation libraries. The base units are:
-///   - Length = [feet/meters]
-///   - Load = [lbs/N]
-///   - Temperature = [deg F / deg C]
+/// supports similar-system unit conversions (ex: meters->millimeters). This is
+/// done so the user can input using preferred units while maintaining
+/// compatibility with the modeling libraries that require consistent units.
+/// The expected consistent units are:
+///   - Length = [meters or feet]
+///   - Load = [N or lbs]
+///   - Temperature = [deg C or deg F]
+///
+/// \par GUI CONTROLS AND DATA TRANSFER
+///
+/// The form controls and layout are defined in the XRC file. The values are
+/// transferred between the controls and the converted cable data via
+/// validators.
 class CableEditorDialog : public wxDialog {
  public:
   /// \brief Default constructor.
@@ -35,16 +35,29 @@ class CableEditorDialog : public wxDialog {
   ///   The cable to be modified with the dialog.
   /// \param[in] units
   ///   The unit system to display on the form.
-  /// \param[in] convert_units
-  ///   The indicator that tells if the units should be converted between the
-  ///   controls and cable.
   CableEditorDialog(wxWindow* parent, Cable* cable,
-                    const UnitSystem& units, const bool& convert_units = true);
+                    const units::UnitSystem& units);
 
   /// \brief Destructor.
   ~CableEditorDialog() {};
 
  private:
+  /// \brief Converts the cable from consistent to dialog form units.
+  /// \param[in] cable_reference
+  ///   The reference cable, which should have dialog units.
+  /// \param[out] cable_converted
+  ///   The converted cable, which will have consistent units.
+  void ConvertCableToConsistentUnits(const Cable& cable_reference,
+                                     Cable& cable_converted);
+
+  /// \brief Converts the cable from consistent to dialog form units.
+  /// \param[in] cable_reference
+  ///   The reference cable, which should have consistent units.
+  /// \param[out] cable_converted
+  ///   The converted cable, which will have dialog units.
+  void ConvertCableToDialogUnits(const Cable& cable_reference,
+                                 Cable& cable_converted);
+
   /// \brief Handles the cancel button event.
   /// \param[in] event
   ///   The event.
@@ -63,25 +76,27 @@ class CableEditorDialog : public wxDialog {
   /// \brief Changes static text to display the desired unit system.
   /// \param[in] units
   ///   The unit system to display.
-  void UpdateUnitsStaticText(UnitSystem& units);
+  void SetUnitsStaticText(units::UnitSystem& units);
 
-  ///// \brief
-  //void TransferControlsDataToCable();
+  /// \brief Sets the control validators on the form.
+  void SetValidators();
 
-  ///// \brief Transfers the cable data to the dialog controls.
-  ///// \param[in]
-  //void TransferCableDataToControls();
-
+  /// \var cable_
+  ///   The cable that is edited.
   Cable* cable_;
 
-  wxString name_;
-  wxString area_electrical_;
-  wxString area_physical_;
-  wxString diameter_;
-  wxString weight_unit_;
+  /// \var cable_converted_
+  ///   The cable that contains the converted (dialog) units. The data stored
+  ///   in this is tied to the dialog controls.
+  Cable cable_converted_;
 
-  UnitSystem units_;
-  bool convert_units_;
+  /// \var name_
+  ///   The control data that contains the cable name.
+  wxString name_;
+
+  /// \var units_
+  ///   The unit system to display and convert to/from.
+  units::UnitSystem units_;
 
   DECLARE_EVENT_TABLE()
 };
--- src/cable_editor_dialog.cc
+++ src/cable_editor_dialog.cc
@@ -3,6 +3,7 @@
 
 #include "cable_editor_dialog.h"
 
+#include "models/base/units.h"
 #include "wx/valnum.h"
 #include "wx/xrc/xmlres.h"
 
@@ -14,73 +15,212 @@ END_EVENT_TABLE()
 
 CableEditorDialog::CableEditorDialog(wxWindow* parent,
                                      Cable* cable,
-                                     const UnitSystem& units,
-                                     const bool& convert_units) { 
-  // set variable
-  cable_ = cable;
-  name_ = wxString(cable_->name);
-
+                                     const units::UnitSystem& units) {
   // loads dialog from virtual xrc file system
   wxXmlResource::Get()->LoadDialog(this, parent, "cable_editor_dialog");
 
-  // updates static text to display selected units
+  // sets static text to display selected units
   units_ = units;
-  UpdateUnitsStaticText(units_);
+  SetUnitsStaticText(units_);
 
-  // set validators on form
+  // transfers and converts cable data to controls data
+  cable_ = cable;
+  ConvertCableToDialogUnits(*cable_, cable_converted_);
 
-  //// cable name
-  //wxTextValidator validator_name = wxTextValidator(wxFILTER_NONE,
-  //                                                 &name_);
-  //XRCCTRL(*this, "textctrl_name", wxTextCtrl)->SetValidator(validator_name);
+  // sets form validators to transfer between controls data and controls
+  SetValidators();
+}
 
-  //// area-electrical
-  //wxFloatingPointValidator<double> validator_area_electrical(
-  //    0,
-  //    &cable_->area_electrical,
-  //    wxNUM_VAL_NO_TRAILING_ZEROES);
-  //XRCCTRL(*this, "textctrl_area_electrical", wxTextCtrl)->SetValidator(
-  //    validator_area_electrical);
+void CableEditorDialog::ConvertCableToConsistentUnits(
+    const Cable& cable_reference,
+    Cable& cable_converted) {
+  // selects based on unit system
+  if (units_ == units::UnitSystem::Metric) {
+    /// \todo
+    ///   Need to convert metric units.
+  } else if (units_ == units::UnitSystem::Imperial) {
+    cable_converted.area_electrical = 0;
+    cable_converted.area_physical = units::Convert(
+        cable_reference.area_physical,
+        units::ConversionType::kInchesToFeet,
+        2);
+    cable_converted.diameter = units::Convert(
+        cable_reference.diameter,
+        units::ConversionType::kInchesToFeet);
+    cable_converted.weight_unit = cable_reference.weight_unit;
+    cable_converted.strength_rated = cable_reference.strength_rated;
+    cable_converted.temperature_properties_components =
+        cable_reference.temperature_properties_components;
+  }
+}
 
-  //// area-physical
-  //wxFloatingPointValidator<double> validator_area_physical(
-  //    4,
-  //    &cable_->area_physical,
-  //    wxNUM_VAL_NO_TRAILING_ZEROES);
-  //XRCCTRL(*this, "textctrl_area_physical", wxTextCtrl)->SetValidator(
-  //    validator_area_physical);
+void CableEditorDialog::ConvertCableToDialogUnits(
+    const Cable& cable_reference,
+    Cable& cable_converted) {
+  // selects based on unit system
+  if (units_ == units::UnitSystem::Metric) {
+    /// \todo
+    ///   Need to convert metric units.
+  } else if (units_ == units::UnitSystem::Imperial) {
+    cable_converted.area_electrical = 0;
+    cable_converted.area_physical = units::Convert(
+        cable_reference.area_physical,
+        units::ConversionType::kFeetToInches,
+        2);
+    cable_converted.diameter = units::Convert(
+        cable_reference.diameter,
+        units::ConversionType::kFeetToInches);
+    cable_converted.weight_unit = cable_reference.weight_unit;
+    cable_converted.strength_rated = cable_reference.strength_rated;
+    cable_converted.temperature_properties_components =
+        cable_reference.temperature_properties_components;
+  }
+}
+void CableEditorDialog::OnClose(wxCloseEvent &event) {
+  EndModal(wxID_CLOSE);
+}
 
-  //// diameter
-  //wxFloatingPointValidator<double> validator_diameter(
-  //    3,
-  //    &cable_->diameter,
-  //    wxNUM_VAL_NO_TRAILING_ZEROES);
-  //XRCCTRL(*this, "textctrl_diameter", wxTextCtrl)->SetValidator(
-  //    validator_diameter);
+void CableEditorDialog::OnCancel(wxCommandEvent &event) {
+  EndModal(wxID_CANCEL);
+}
 
-  //// weight-unit
-  //wxFloatingPointValidator<double> validator_weight_unit(
-  //    3,
-  //    &cable_->weight_unit,
-  //    wxNUM_VAL_NO_TRAILING_ZEROES);
-  //XRCCTRL(*this, "textctrl_weight_unit", wxTextCtrl)->SetValidator(
-  //    validator_weight_unit);
+void CableEditorDialog::OnOk(wxCommandEvent &event) {
+  // validates data from form
+  if (this->Validate() == false) {
+    wxMessageBox("Errors on form");
+    return;
+  }
 
-  //// strength-rated
-  //wxFloatingPointValidator<double> validator_strength_rated(
-  //    0,
-  //    &cable_->strength_rated,
-  //    wxNUM_VAL_THOUSANDS_SEPARATOR);
-  //XRCCTRL(*this, "textctrl_strength_rated", wxTextCtrl)->SetValidator(
-  //    validator_strength_rated);
+  // transfers data from dialog controls to converted cable
+  this->TransferDataFromWindow();
+  cable_converted_.name = name_.ToStdString();
 
-  //// temperature-properties
-  //wxFloatingPointValidator<double> validator_temperature_properties(
-  //    0,
-  //    &cable_->temperature_properties_components,
-  //    wxNUM_VAL_NO_TRAILING_ZEROES);
-  //XRCCTRL(*this, "textctrl_temperature_properties", wxTextCtrl)->SetValidator(
-  //    validator_temperature_properties);
+  //// validates model data
+  //// creates a temp cable for data validation
+  //Cable cable_temp;
+  //ConvertCableToConsistentUnits(cable_converted_, cable_temp);
+  //cable_temp.type_polynomial_active = CableComponent::PolynomialType::kLoadStrain; // TEMPORARY
+  //std::list<std::string> messages_error;
+  //if (cable_temp.Validate(true, &messages_error) == true) {
+  //  
+  //  // updates original data reference based on user form edits
+  //  ConvertCableToConsistentUnits(cable_converted_, *cable_);
+
+  //  // ends modal by returning ok indicator
+    EndModal(wxID_OK);
+  //} else {
+  //  /// \todo
+  //  /// do something to notify user of the errors
+  //  wxMessageDialog message(this, "Errors present");
+  //}
+}
+
+void CableEditorDialog::SetUnitsStaticText(units::UnitSystem& units) {
+
+  if (units == units::UnitSystem::Metric) {
+    /// \todo
+    /// Need to include metric form labeling.
+    XRCCTRL(*this, "statictext_area_electrical_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_area_physical_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_diameter_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_weight_unit_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_strength_rated_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_temperature_properties_units", wxStaticText)->
+        SetLabel("???");
+
+    XRCCTRL(*this, "statictext_shell_coefficient_expansion_thermal_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_shell_modulus_tension_units", wxStaticText)->
+        SetLabel("???");
+
+    XRCCTRL(*this, "statictext_core_coefficient_expansion_thermal_units", wxStaticText)->
+        SetLabel("???");
+    XRCCTRL(*this, "statictext_core_modulus_tension_units", wxStaticText)->
+        SetLabel("???");
+  } else if (units == units::UnitSystem::Imperial) {
+    XRCCTRL(*this, "statictext_area_electrical_units", wxStaticText)->
+        SetLabel("[kcmil]");
+    XRCCTRL(*this, "statictext_area_physical_units", wxStaticText)->
+        SetLabel("[in^2]");
+    XRCCTRL(*this, "statictext_diameter_units", wxStaticText)->
+        SetLabel("[in]");
+    XRCCTRL(*this, "statictext_weight_unit_units", wxStaticText)->
+        SetLabel("[lb/ft]");
+    XRCCTRL(*this, "statictext_strength_rated_units", wxStaticText)->
+        SetLabel("[lbs]");
+    XRCCTRL(*this, "statictext_temperature_properties_units", wxStaticText)->
+        SetLabel("[degF]");
+
+    XRCCTRL(*this, "statictext_shell_coefficient_expansion_thermal_units", wxStaticText)->
+        SetLabel("[degF]");
+    XRCCTRL(*this, "statictext_shell_modulus_tension_units", wxStaticText)->
+        SetLabel("[lb/in^2]");
+
+    XRCCTRL(*this, "statictext_core_coefficient_expansion_thermal_units", wxStaticText)->
+        SetLabel("[degF]");
+    XRCCTRL(*this, "statictext_core_modulus_tension_units", wxStaticText)->
+        SetLabel("[lb/in^2]");
+  }
+}
+
+void CableEditorDialog::SetValidators() {
+  // cable name
+  wxTextValidator validator_name = wxTextValidator(wxFILTER_NONE,
+                                                   &name_);
+  XRCCTRL(*this, "textctrl_name", wxTextCtrl)->SetValidator(validator_name);
+
+  // area-electrical
+  wxFloatingPointValidator<double> validator_area_electrical(
+      0,
+      &cable_converted_.area_electrical,
+      wxNUM_VAL_NO_TRAILING_ZEROES);
+  XRCCTRL(*this, "textctrl_area_electrical", wxTextCtrl)->SetValidator(
+      validator_area_electrical);
+
+  // area-physical
+  wxFloatingPointValidator<double> validator_area_physical(
+      4,
+      &cable_converted_.area_physical,
+      wxNUM_VAL_NO_TRAILING_ZEROES);
+  XRCCTRL(*this, "textctrl_area_physical", wxTextCtrl)->SetValidator(
+      validator_area_physical);
+
+  // diameter
+  wxFloatingPointValidator<double> validator_diameter(
+      3,
+      &cable_converted_.diameter,
+      wxNUM_VAL_NO_TRAILING_ZEROES);
+  XRCCTRL(*this, "textctrl_diameter", wxTextCtrl)->SetValidator(
+      validator_diameter);
+
+  // weight-unit
+  wxFloatingPointValidator<double> validator_weight_unit(
+      3,
+      &cable_converted_.weight_unit,
+      wxNUM_VAL_NO_TRAILING_ZEROES);
+  XRCCTRL(*this, "textctrl_weight_unit", wxTextCtrl)->SetValidator(
+      validator_weight_unit);
+
+  // strength-rated
+  wxFloatingPointValidator<double> validator_strength_rated(
+      0,
+      &cable_converted_.strength_rated,
+      wxNUM_VAL_THOUSANDS_SEPARATOR);
+  XRCCTRL(*this, "textctrl_strength_rated", wxTextCtrl)->SetValidator(
+      validator_strength_rated);
+
+  // temperature-properties
+  wxFloatingPointValidator<double> validator_temperature_properties(
+      0,
+      &cable_converted_.temperature_properties_components,
+      wxNUM_VAL_NO_TRAILING_ZEROES);
+  XRCCTRL(*this, "textctrl_temperature_properties", wxTextCtrl)->SetValidator(
+      validator_temperature_properties);
 
   //// shell coefficient-expansion-thermal
   //wxFloatingPointValidator<double> validator_shell_coefficient_expansion_thermal(
@@ -193,57 +333,4 @@ CableEditorDialog::CableEditorDialog(wxWindow* parent,
   //    wxNUM_VAL_NO_TRAILING_ZEROES);
   //XRCCTRL(*this, "textctrl_core_a4", wxTextCtrl)->SetValidator(
   //    validator_core_a4);
-}
-
-void CableEditorDialog::OnClose(wxCloseEvent &event) {
-  EndModal(wxID_CLOSE);
-}
-
-void CableEditorDialog::OnCancel(wxCommandEvent &event) {
-  EndModal(wxID_CANCEL);
-}
-
-void CableEditorDialog::OnOk(wxCommandEvent &event) {
-  // validates data from form
-  if (this->Validate() == false) {
-    wxMessageBox("Errors on form");
-    return;
-  }
-
-  // transfers data from dialog controls to cable
-  this->TransferDataFromWindow();
-  cable_->name = name_.ToStdString();
-
-  // ends modal by returning ok indicator
-  EndModal(wxID_OK);
-}
-
-void CableEditorDialog::UpdateUnitsStaticText(UnitSystem& units) {
-
-  if (units == UnitSystem::Metric) {
-
-  } else {
-    XRCCTRL(*this, "statictext_area_electrical_units", wxStaticText)->
-        SetLabel("[kcmil]");
-    XRCCTRL(*this, "statictext_area_physical_units", wxStaticText)->
-        SetLabel("[in^2]");
-    XRCCTRL(*this, "statictext_diameter_units", wxStaticText)->
-        SetLabel("[in]");
-    XRCCTRL(*this, "statictext_weight_unit_units", wxStaticText)->
-        SetLabel("[lb/ft]");
-    XRCCTRL(*this, "statictext_strength_rated_units", wxStaticText)->
-        SetLabel("[lbs]");
-    XRCCTRL(*this, "statictext_temperature_properties_units", wxStaticText)->
-        SetLabel("[degF]");
-
-    XRCCTRL(*this, "statictext_shell_coefficient_expansion_thermal_units", wxStaticText)->
-        SetLabel("[degF]");
-    XRCCTRL(*this, "statictext_shell_modulus_tension_units", wxStaticText)->
-        SetLabel("[lb/in^2]");
-
-    XRCCTRL(*this, "statictext_core_coefficient_expansion_thermal_units", wxStaticText)->
-        SetLabel("[degF]");
-    XRCCTRL(*this, "statictext_core_modulus_tension_units", wxStaticText)->
-        SetLabel("[lb/in^2]");
-  }
 }
