6b2c108eebd1aabb0eb668a034d180c4f6351c31
bretzel12 <bretzel12@users.noreply.github.com>
2016-12-15 15:37:06 -0800

    Added Vector2dXmlHandler.

--- src/vector_xml_handler.cc
+++ src/vector_xml_handler.cc
@@ -5,6 +5,116 @@
 
 #include "models/base/helper.h"
 
+Vector2dXmlHandler::Vector2dXmlHandler() {
+}
+
+Vector2dXmlHandler::~Vector2dXmlHandler() {
+}
+
+wxXmlNode* Vector2dXmlHandler::CreateNode(
+    const Vector2d& vector,
+    const std::string& name,
+    const wxXmlAttribute& attribute_components,
+    const int& precision) {
+  // variables used to create XML node
+  wxXmlNode* node_root = nullptr;
+  wxXmlNode* node_element = nullptr;
+  std::string title;
+  std::string content;
+  double value;
+
+  // creates a node for the cable constraint root
+  node_root = new wxXmlNode(wxXML_ELEMENT_NODE, "vector_2d");
+  node_root->AddAttribute("version", "1");
+  node_root->AddAttribute("name", name);
+
+  // adds child nodes
+
+  // creates x node and adds to parent node
+  title = "x";
+  value = vector.x();
+  content = helper::DoubleToFormattedString(value, precision);
+  node_element = CreateElementNodeWithContent(title, content,
+                                              &attribute_components);
+  node_root->AddChild(node_element);
+
+  // creates y node and adds to parent node
+  title = "y";
+  value = vector.y();
+  content = helper::DoubleToFormattedString(value, precision);
+  node_element = CreateElementNodeWithContent(title, content,
+                                              &attribute_components);
+  node_root->AddChild(node_element);
+
+  // returns node
+  return node_root;
+}
+
+int Vector2dXmlHandler::ParseNode(const wxXmlNode* root,
+                                  const std::string& filepath,
+                                  Vector2d& vector) {
+  // checks for valid root node
+  if (root->GetName() != "vector_2d") {
+    return root->GetLineNumber();
+  }
+
+  // gets version attribute
+  wxString version;
+  if (root->GetAttribute("version", &version) == false) {
+    return root->GetLineNumber();
+  }
+
+  // sends to proper parsing function
+  if (version == "1") {
+    return ParseNodeV1(root, filepath, vector);
+  } else {
+    return root->GetLineNumber();
+  }
+}
+
+int Vector2dXmlHandler::ParseNodeV1(const wxXmlNode* root,
+                                    const std::string& filepath,
+                                    Vector2d& vector) {
+  wxString message;
+
+  // evaluates each child node
+  wxXmlNode* node = root->GetChildren();
+  while (node != nullptr) {
+    const wxString title = node->GetName();
+    const wxString content = ParseElementNodeWithContent(node);
+    double value = -999999;
+
+    if (title == "x") {
+      if (content.ToDouble(&value) == true) {
+        vector.set_x(value);
+      } else {
+        message = FileAndLineNumber(filepath, node)
+                  + "Invalid x component.";
+        wxLogError(message);
+        vector.set_x(-999999);
+      }
+    } else if (title == "y") {
+      if (content.ToDouble(&value) == true) {
+        vector.set_y(value);
+      } else {
+        message = FileAndLineNumber(filepath, node)
+                  + "Invalid y component.";
+        wxLogError(message);
+        vector.set_y(-999999);
+      }
+    } else {
+      message = FileAndLineNumber(filepath, node)
+                + "XML node isn't recognized.";
+      wxLogError(message);
+    }
+
+    node = node->GetNext();
+  }
+
+  // if it gets to this point, no critical errors were encountered
+  return 0;
+}
+
 Vector3dXmlHandler::Vector3dXmlHandler() {
 }
 
@@ -46,7 +156,7 @@ wxXmlNode* Vector3dXmlHandler::CreateNode(
                                               &attribute_components);
   node_root->AddChild(node_element);
 
-  // creates y node and adds to parent node
+  // creates z node and adds to parent node
   title = "z";
   value = vector.z();
   content = helper::DoubleToFormattedString(value, precision);
