f95489c1369d3ad1cebea40658dbd50d799f07e3
bretzel12 <bretzel12@users.noreply.github.com>
2016-12-19 12:34:03 -0800

    Added LogPane functionality.
    - Added a custom wxTextCtrl for the log pane.
    - Instead of displaying default wxTextCtrl menu, a custom menu is
      displayed that allows the log to be cleared or copyied to the
      clipboard.

--- src/log_pane.cc
+++ src/log_pane.cc
@@ -3,10 +3,67 @@
 
 #include "log_pane.h"
 
+#include "wx/clipbrd.h"
 #include "wx/xrc/xmlres.h"
 
-BEGIN_EVENT_TABLE(LogPane, wxPanel)
+/// context menu enum
+enum {
+  kClear = 0,
+  kCopyToClipboard = 1
+};
+
+BEGIN_EVENT_TABLE(LogPaneTextCtrl, wxPanel)
+  EVT_MENU(wxID_ANY, LogPaneTextCtrl::OnContextMenuSelect)
+  EVT_RIGHT_DOWN(LogPaneTextCtrl::OnMouse)
 END_EVENT_TABLE()
+
+LogPaneTextCtrl::LogPaneTextCtrl(wxWindow* parent)
+  : wxTextCtrl(parent, wxID_ANY, wxEmptyString, wxDefaultPosition,
+               wxSize(600, 400), wxTE_MULTILINE | wxTE_READONLY) {
+}
+
+LogPaneTextCtrl::~LogPaneTextCtrl() {
+}
+
+void LogPaneTextCtrl::CopyLogToClipboard() {
+  if (wxTheClipboard->Open()) {
+    // gets the textctrl contents
+    std::string str = GetValue();
+
+    // creates a text data object to store clipboard information
+    wxTextDataObject* data = new wxTextDataObject();
+    data->SetText(str);
+
+    // copies to the clipboard and closes
+    wxTheClipboard->SetData(data);
+    wxTheClipboard->Close();
+  }
+}
+
+void LogPaneTextCtrl::OnContextMenuSelect(wxCommandEvent& event) {
+  // gets context menu selection and sends to handler function
+  const int id_event = event.GetId();
+  if (id_event == kClear) {
+    Clear();
+  } else if (id_event == kCopyToClipboard) {
+    CopyLogToClipboard();
+  }
+}
+
+void LogPaneTextCtrl::OnMouse(wxMouseEvent& event) {
+  if (event.RightDown() == true) {
+    // builds a context menu
+    wxMenu menu;
+
+    menu.Append(kClear, "Clear");
+    menu.Append(kCopyToClipboard, "Copy To Clipboard");
+
+    // shows context menu
+    // the event is caught by the pane
+    PopupMenu(&menu, event.GetPosition());
+  }
+}
+
 
 /// \todo This pane should eventually be declared in an XRC file.
 ///    wxWidgets is throwing an assert when specifying a multline style,
@@ -14,11 +71,11 @@ END_EVENT_TABLE()
 LogPane::LogPane(wxWindow* parent)
     : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize,
               wxTAB_TRAVERSAL) {
-  // creates a textctrl and adds to pane
+  // creates textctrl
+  textctrl_ = new LogPaneTextCtrl(this);
+
+  // adds to pane and fits
   wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-  textctrl_ = new wxTextCtrl(this, wxID_ANY, wxEmptyString,
-                             wxDefaultPosition, wxSize(600, 400),
-                             wxTE_MULTILINE|wxTE_READONLY);
   sizer->Add(textctrl_, 1, wxEXPAND);
   this->SetSizerAndFit(sizer);
 }
@@ -27,5 +84,5 @@ LogPane::~LogPane() {
 }
 
 wxTextCtrl* LogPane::textctrl() {
-  return textctrl_;
+  return (wxTextCtrl*)textctrl_;
 }
