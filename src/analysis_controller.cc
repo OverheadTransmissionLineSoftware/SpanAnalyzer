ff122af3965dc7b2b7163b0a9e1f2093a741e97a
bretzel12 <bretzel12@users.noreply.github.com>
2017-06-12 18:42:23 -0700

    AnalysisController updated with multi-threading.
    - Helps remove calculation bottleneck in main thread by
      temporarily using the whole potential of the cpu.
    - Created custom jobs and threads to coordinate the analysis. The
      detailed analysis parts were offloaded from the controller class.
    - Threads are 'joinable' to keep things easy and in sync with the
      main thread.

--- src/analysis_controller.cc
+++ src/analysis_controller.cc
@@ -8,12 +8,132 @@
 #include "models/base/helper.h"
 #include "wx/wx.h"
 
+AnalysisThread::AnalysisThread() : wxThread(wxTHREAD_JOINABLE) {
+}
+
+void AnalysisThread::AddAnalysisJob(AnalysisJob* job) {
+  jobs_.push_back(job);
+}
+
+const LineCable* AnalysisThread::line_cable() const {
+  return line_cable_;
+}
+
+const std::list<ErrorMessage>* AnalysisThread::messages() const {
+  return &messages_;
+}
+
+const LineCableReloader* AnalysisThread::reloader() const {
+  return &reloader_;
+}
+
+void AnalysisThread::set_line_cable(const LineCable* line_cable) {
+  line_cable_ = line_cable;
+}
+
+void AnalysisThread::DoAnalysisJob(const int& index) {
+  std::string message;
+  AnalysisJob* job = *std::next(jobs_.begin(), index);
+
+  // sets up reloader
+  reloader_.set_condition_reloaded(job->condition);
+  reloader_.set_weathercase_reloaded(job->weathercase);
+
+  // validates reloader and logs any errors
+  std::list<ErrorMessage> messages;
+  const bool status = reloader_.Validate(false, &messages);
+  if (status == false) {
+    // errors were present
+    // adds analysis controller error message to give context
+    ErrorMessage message;
+    message.title = "ANALYSIS THREAD";
+    if (job->condition == CableConditionType::kCreep) {
+      message.description = "No solution for "
+                            + job->weathercase->description
+                            + " Creep.";
+    } else if (job->condition == CableConditionType::kInitial) {
+      message.description = "No solution for "
+                            + job->weathercase->description
+                            + " Initial.";
+    } else if (job->condition == CableConditionType::kLoad) {
+      message.description = "No solution for "
+                            + job->weathercase->description
+                            + " Load.";
+    }
+
+    messages_.push_back(message);
+
+    // adds reloader error messages
+    messages_.splice(messages_.cend(), messages);
+  }
+
+  // populates result
+  SagTensionAnalysisResult& result = *job->result;
+  result.weathercase = job->weathercase;
+
+  if (status == true) {
+    // no errors were present
+    result.condition = job->condition;
+
+    // gets reloaded catenary and populates sag-tension result
+    Catenary3d catenary = reloader_.CatenaryReloaded();
+
+    result.tension_average = catenary.TensionAverage();
+    result.tension_average_core = reloader_.TensionAverageComponent(
+        CableElongationModel::ComponentType::kCore);
+    result.tension_average_shell = reloader_.TensionAverageComponent(
+        CableElongationModel::ComponentType::kShell);
+
+    result.tension_horizontal = catenary.tension_horizontal();
+    result.tension_horizontal_core = reloader_.TensionHorizontalComponent(
+        CableElongationModel::ComponentType::kCore);
+    result.tension_horizontal_shell = reloader_.TensionHorizontalComponent(
+        CableElongationModel::ComponentType::kShell);
+
+    result.weight_unit = catenary.weight_unit();
+
+    result.state = reloader_.StateReloaded();
+  } else {
+    // flags this as an invalid result
+    result.condition = CableConditionType::kNull;
+
+    result.tension_average = -999999;
+    result.tension_average_core = -999999;
+    result.tension_average_shell = -999999;
+
+    result.tension_horizontal = -999999;
+    result.tension_horizontal_core = -999999;
+    result.tension_horizontal_shell = -999999;
+
+    result.weight_unit = Vector3d();
+
+    result.state = CableStretchState();
+  }
+}
+
+wxThread::ExitCode AnalysisThread::Entry() {
+  // initializes reloader
+  reloader_.set_line_cable(line_cable_);
+
+  // does all jobs in the list
+  const int kSizeJobs = jobs_.size();
+  for (int i = 0; i < kSizeJobs; i++) {
+    DoAnalysisJob(i);
+  }
+
+  // exits
+  return (wxThread::ExitCode)0;
+}
+
+
 AnalysisController::AnalysisController() {
   span_ = nullptr;
   weathercases_ = nullptr;
 
-  reloader_.set_line_cable(nullptr);
-  reloader_.set_weathercase_reloaded(nullptr);
+  max_threads_ = wxThread::GetCPUCount();
+  if (max_threads_ == -1) {
+    max_threads_ = 1;
+  }
 
   state_stretch_initial_.load = 0;
   state_stretch_initial_.temperature = 0;
@@ -65,11 +185,8 @@ const std::vector<SagTensionAnalysisResult>* AnalysisController::Results(
   }
 }
 
-void AnalysisController::RunAnalysis() const {
-  // clears cached results
-  results_creep_.clear();
-  results_initial_.clear();
-  results_load_.clear();
+void AnalysisController::RunAnalysis() {
+  std::string message;
 
   // checks if span has been selected
   if (span_ == nullptr) {
@@ -88,55 +205,143 @@ void AnalysisController::RunAnalysis() const {
       wxLogError(message.c_str());
     }
 
-    wxLogVerbose("Span validation errors are present. Aborting analysis.");
+    wxLogError("Span validation errors are present. Aborting analysis.");
     status_bar_log::SetText("Span validation error(s) present, see logs", 0);
 
     return;
   }
 
-  wxLogVerbose("Running sag-tension analysis.");
-  status_bar_log::PushText("Running sag-tension analysis...", 0);
+  // clears cached results
+  results_creep_.clear();
+  results_initial_.clear();
+  results_load_.clear();
+  state_stretch_creep_ = CableStretchState();
+  state_stretch_load_ = CableStretchState();
 
-  // creates a timer and records start time
+  // starts analysis timer
   Timer timer;
   timer.Start();
 
-  // sets up reloader
-  reloader_.set_line_cable(&span_->linecable);
+  // creates empty set of results that will be populated by worker threads
+  results_creep_.resize(weathercases_->size(), SagTensionAnalysisResult());
+  results_initial_.resize(weathercases_->size(), SagTensionAnalysisResult());
+  results_load_.resize(weathercases_->size(), SagTensionAnalysisResult());
 
-  // runs analysis for each weathercase
+  // creates a job list
+  std::list<AnalysisJob> jobs;
   for (auto iter = weathercases_->cbegin(); iter != weathercases_->cend();
        iter++) {
-    const WeatherLoadCase* weathercase = *iter;
+    const int index = std::distance(weathercases_->cbegin(), iter);
 
-    SagTensionAnalysisResult result;
+    AnalysisJob job;
+    job.weathercase = *iter;
 
-    // appends result for creep condition
-    result = Analyze(*weathercase, CableConditionType::kCreep);
-    results_creep_.push_back(result);
+    job.condition = CableConditionType::kCreep;
+    job.result = &results_creep_[index];
+    jobs.push_back(job);
 
-    // appends a result for initial condition
-    result = Analyze(*weathercase, CableConditionType::kInitial);
-    results_initial_.push_back(result);
+    job.condition = CableConditionType::kInitial;
+    job.result = &results_initial_[index];
+    jobs.push_back(job);
 
-    // appends a result for load condition
-    result = Analyze(*weathercase, CableConditionType::kLoad);
-    results_load_.push_back(result);
+    job.condition = CableConditionType::kLoad;
+    job.result = &results_load_[index];
+    jobs.push_back(job);
+  }
+
+  // determines the number of analysis threads to use
+  int num_threads = -1;
+  const int num_jobs = jobs.size();
+
+  if (num_jobs < max_threads_) {
+    num_threads = num_jobs;
+  } else {
+    num_threads = max_threads_;
+  }
+
+  // creates analysis threads
+  std::list<AnalysisThread*> threads;
+  for (int i = 0; i < num_threads; i++) {
+    AnalysisThread* thread = new AnalysisThread();
+    thread->set_line_cable(&span_->linecable);
+    threads.push_back(thread);
+  }
+
+  // adds jobs to threads round robin style
+  auto iter_jobs = jobs.begin();
+  while (iter_jobs != jobs.end()) {
+    for (auto iter = threads.begin(); iter != threads.end(); iter++) {
+      AnalysisThread* thread = *iter;
+
+      if (iter_jobs == jobs.cend()) {
+        break;
+      } else {
+        AnalysisJob* job = &(*iter_jobs);
+        thread->AddAnalysisJob(job);
+
+        iter_jobs++;
+      }
+    }
+  }
+
+  // logs analysis start
+  message = "Calculating " + std::to_string(num_jobs)
+            + " sag-tension solutions using "
+            + std::to_string(num_threads) + " threads.";
+  wxLogVerbose(message.c_str());
+  status_bar_log::PushText("Running sag-tension analysis...", 0);
+
+  // starts threads
+  for (auto iter = threads.begin(); iter != threads.end(); iter++) {
+    AnalysisThread* thread = *iter;
+    wxThreadError status = thread->Run();
+    if (status != wxTHREAD_NO_ERROR) {
+      wxLogError("Couldn't start analysis thread");
+    }
+  }
+
+  // waits for threads to complete (i.e. joins worker threads back into main)
+  bool is_errors = false;
+  for (auto iter_thread = threads.begin(); iter_thread != threads.end();
+       iter_thread++) {
+    AnalysisThread* thread = *iter_thread;
+    thread->Wait();
+
+    // collects any thread errors and logs
+    const std::list<ErrorMessage>* messages = thread->messages();
+    for (auto iter_message = messages->cbegin();
+         iter_message != messages->cend(); iter_message++) {
+      is_errors = true;
+      const ErrorMessage& message_error = *iter_message;
+      std::string str = message_error.title + " - " + message_error.description;
+      wxLogError(str.c_str());
+    }
   }
 
   // updates stretch states
-  state_stretch_creep_ = reloader_.StretchStateCreep();
-  state_stretch_load_ = reloader_.StretchStateLoad();
+  // the stretch states are similar for all threads, so it grabs from first one
+  AnalysisThread* thread = *threads.cbegin();
+  state_stretch_creep_ = thread->reloader()->StretchStateCreep();
+  state_stretch_load_ = thread->reloader()->StretchStateLoad();
 
-  // un-inits reloader
-  reloader_.set_line_cable(&span_->linecable);
+  // deletes threads
+  for (auto iter = threads.begin(); iter != threads.end(); iter++) {
+    AnalysisThread* thread = *iter;
+    delete thread;
+  }
 
   // stops timer and logs
   timer.Stop();
-  std::string message = "Analysis time = "
-                        + helper::DoubleToFormattedString(timer.Duration(), 3)
-                        + "s.";
+  message = "Analysis time = "
+            + helper::DoubleToFormattedString(timer.Duration(), 3) + "s.";
   wxLogVerbose(message.c_str());
+
+  // notifies user of any errors
+  if (is_errors == true) {
+    // notifies user of error
+    message = "Analysis encountered error(s). Check logs.";
+    wxMessageBox(message);
+  }
 
   // clears status bar
   status_bar_log::PopText(0);
@@ -176,59 +381,4 @@ const Span* AnalysisController::span() const {
 
 const std::list<WeatherLoadCase*>* AnalysisController::weathercases() const {
   return weathercases_;
-}
-
-SagTensionAnalysisResult AnalysisController::Analyze(
-    const WeatherLoadCase& weathercase,
-    const CableConditionType& condition) const {
-  // initializes
-  SagTensionAnalysisResult result;
-
-  // updates reloader
-  reloader_.set_condition_reloaded(condition);
-  reloader_.set_weathercase_reloaded(&weathercase);
-
-  // validates reloader and logs any errors
-  std::list <ErrorMessage> errors;
-  if (reloader_.Validate(false, &errors) == false) {
-    std::string message;
-    if (condition == CableConditionType::kInitial) {
-      message = "Reloader error for " + weathercase.description
-                + " weathercase when determining the Initial condition.";
-    } else if (condition == CableConditionType::kLoad) {
-      message = "Reloader error for " + weathercase.description
-                + " weathercase when determining the Load condition.";
-    }
-    wxLogError(message.c_str());
-
-    for (auto iter = errors.cbegin(); iter != errors.cend(); iter++) {
-      const ErrorMessage& error = *iter;
-      std::string message = error.title + "  --  " + error.description;
-      wxLogError(message.c_str());
-    }
-
-    return result;
-  }
-
-  // gets reloaded catenary and populates sag-tension result
-  Catenary3d catenary = reloader_.CatenaryReloaded();
-
-  result.tension_average = catenary.TensionAverage();
-  result.tension_average_core = reloader_.TensionAverageComponent(
-      CableElongationModel::ComponentType::kCore);
-  result.tension_average_shell = reloader_.TensionAverageComponent(
-      CableElongationModel::ComponentType::kShell);
-
-  result.tension_horizontal = catenary.tension_horizontal();
-  result.tension_horizontal_core = reloader_.TensionHorizontalComponent(
-      CableElongationModel::ComponentType::kCore);
-  result.tension_horizontal_shell = reloader_.TensionHorizontalComponent(
-      CableElongationModel::ComponentType::kShell);
-  result.weight_unit = catenary.weight_unit();
-
-  result.condition = condition;
-  result.state = reloader_.StateReloaded();
-  result.weathercase = &weathercase;
-
-  return result;
 }
