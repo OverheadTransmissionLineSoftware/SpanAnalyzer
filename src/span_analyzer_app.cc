37d5f1f849f4c11d83f767b5539845c18ddc371b
bretzel12 <bretzel12@users.noreply.github.com>
2019-03-24 18:06:06 -0700

    Version updated to 2.0.0.

--- src/span_analyzer_app.cc
+++ src/span_analyzer_app.cc
@@ -118,7 +118,7 @@ bool SpanAnalyzerApp::OnInit() {
   // sets application info
   SetAppDisplayName("SpanAnalyzer");
   SetAppName("SpanAnalyzer");
-  version_ = "1.1.0";
+  version_ = "2.0.0";
 
   // creates a document manager and sets to single document interface
   manager_doc_ = new wxDocManager();
